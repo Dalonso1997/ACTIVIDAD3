c31f54dc3983c50da420dba42497dcf48d9fa86d
Dalonso1997 <david@David>
2025-12-03 23:04:52 +0100

    Añadida traduccion de la serie de fibonacci

---
+++ SerieFibonacci/src/main/java/com/mycompany/seriefibonacci/SerieFibonacci.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seriefibonacci;
+
+/**
+ *
+ * @author david
+ */
+public class SerieFibonacci {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
